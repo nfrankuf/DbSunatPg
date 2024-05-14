5737e207cb2c94d4c06254f90af7700dd33a7281
nfrankuf <nushinahuaf@gmail.com>
2024-05-14 23:18:16 +0200

    Load More Data

--- SQLQueryCreateTables.sql
+++ SQLQueryCreateTables.sql
@@ -1,6 +1,14 @@
+USE PESunatDB
+GO
 
 CREATE TABLE Table01TipoDocumento(
 	Idtd	int identity(1,1) not null primary key,
 	Codtd	char(8),
 	Glosatd	varchar(255)
+)
+
+CREATE TABLE Table25CodigoUNSPSC(
+	IdCod		int identity(1,1) not null primary key,
+	CodUns		char(8),
+	GlosaUns	varchar(255)
 )
--- SQLQueryLoadDataTables.sql
+++ SQLQueryLoadDataTables.sql
@@ -3,11 +3,18 @@ GO
 
 --Load data from Table	========>	Table01TipoDocumento
 BULK INSERT	Table01TipoDocumento
-FROM	'C:\tmpPGQL\Tabla01_CatalogoTipoDocumento.txt'
-	--	'C:\Users\satellite\Documents\GitHub\DbSunatPg\TextFiles\Tabla01_CatalogoTipoDocumento.txt'
+FROM	--'C:\tmpPGQL\Tabla01_CatalogoTipoDocumento.txt'
+	'C:\Users\satellite\Documents\GitHub\DbSunatPg\TextFiles\Tabla01_CatalogoTipoDocumento.txt'
 WITH	(
 			CODEPAGE		='65001',
 			FIELDTERMINATOR	='|')
 GO
 
-
+--Load data from Table	========>	Table25CodigoUNSPSC
+BULK INSERT	Table25CodigoUNSPSC
+FROM	'C:\tmpPGQL\Tabla25_CatalogoUNSPSC.txt'
+		--'C:\Users\satellite\Documents\GitHub\DbSunatPg\TextFiles\Tabla01_CatalogoTipoDocumento.txt'
+WITH	(
+			CODEPAGE		='65001',
+			FIELDTERMINATOR	='|')
+GO
